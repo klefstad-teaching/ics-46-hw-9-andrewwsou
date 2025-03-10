a170882c2d25b6cacff33d96c78bbbc598174657
Andrew Sou <aksou@uci.edu>
2025-03-10 08:00:41 +0000

    Added printing fix for labeling ladders

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -94,12 +94,14 @@ void load_words(set<string> & word_list, const string& file_name) {
 
 void print_word_ladder(const vector<string>& ladder) {
     if (!ladder.empty()) {
+        cout << "Word ladder found: ";
         for (size_t i = 0; i < ladder.size(); ++i) {
             cout << ladder[i] << " ";
         }
         cout << endl;
+    } else {
+        cout << "No word ladder found." << endl;
     }
-    return;
 }
 
 
