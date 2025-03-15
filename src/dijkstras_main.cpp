d9f5b223768e59f6e459493fefb4c3fc34cae680
Andrew Sou <aksou@uci.edu>
2025-03-15 01:48:07 +0000

    Added formatting fixes

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -1,14 +1,5 @@
 #include "dijkstras.h"
 
 int main() {
-    Graph g;
-    file_to_graph("small.txt", g);
-
-    int source = 0;
-    vector<int> previous;
-    vector<int> distances = dijkstra_shortest_path(g, source, previous);
-
-    for (int i = 0; i < g.numVertices; ++i){
-        print_path(extract_shortest_path(distances, previous, i), distances[i]);
-    }
+    //
 }
