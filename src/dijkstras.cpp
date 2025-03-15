728e81a60640005f1a5b9c5e853f1d849690fda0
Andrew Sou <aksou@uci.edu>
2025-03-15 01:40:15 +0000

    Added error check that would return nothing in graph. Still prints out weight even if no distances

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -51,9 +51,6 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 }
 
 void print_path(const vector<int>& v, int total) {
-    if (v.empty()) {
-        return;
-    }
     for (size_t i = 0; i < v.size(); ++i) {
         cout << v[i] << " ";
     }
